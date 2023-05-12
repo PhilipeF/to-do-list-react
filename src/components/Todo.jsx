6d1c94b5d3388662319d92e96eaa9511864c4f55
rick <philipesferreiraa@gmail.com>
2023-05-12 12:38:38 -0300

    function completo to-do   ok

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
-import { useState } from 'react'
-
-const Todo = ({ todo }) => {
-
-    
+const Todo = ({ todo, removeTodo, completeTodo }) => {
 
     return (
         <div className="todo">
-            <div className="content">
+            <div className="content"
+                style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+            >
                 <p>{todo.text}</p>
                 <p>({todo.category})</p>
             </div>
             <div className="button">
-                <button className='complete'>Completar</button>
-                <button className='remove'>x</button>
+                <button className='complete'
+                    onClick={() => completeTodo(todo.id)}
+                >
+                    Completar
+                </button>
+                <button className='remove'
+                    onClick={() => removeTodo(todo.id)}
+                >
+                    x
+                </button>
             </div>
         </div>
     )
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+
+    const [value, setValue] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        if (!value || !category) return
+        addTodo(value, category)
+        setValue('')
+        setCategory('')
+    }
+
     return (
         <div className="todo-form">
             <h2>Criar tarefa:</h2>
-            <form>
-                <input type="text" name="Criar uma nova tarefa" />
-                <select>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="Criar uma nova tarefa"
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                />
+                <select
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}>
                     <option value="">Selecione uma categoria</option>
                     <option value="Trabalho">Trabalho</option>
                     <option value="Estudo">Estudo</option>
