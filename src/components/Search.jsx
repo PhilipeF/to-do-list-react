1896e593fd0764632c1c17935360fe0606c30ff3
rick <philipesferreiraa@gmail.com>
2023-05-19 18:27:23 -0300

    funcionalidade modo dark-light 80% ok

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 
+const body = document.querySelector('body')
+const modeSwitch = document.querySelector('.toggle-switch')
+
+const handleCLick = () => {
+    console.log('Passei aqui')
+    body.classList.toggle('dark')
+}
+
 export const Search = ({ search, setSearch }) => {
     return (
-        <div className='search'>
-            <h2>Pesquisar:</h2>
-            <input
-                type="text"
-                placeholder='Digite aqui'
-                value={search}
-                onChange={(event) => setSearch(event.target.value)}
-                autoComplete='off'
-            />
+        <div className="search">
+            <div className="topo">
+                <h2>Pesquisar:</h2>
+                <div className="toggle-switch" onClick={() => handleCLick()}>
+                    <span className="switch"></span>
+                </div>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    placeholder='Digite aqui'
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                    autoComplete='off'
+                />
+            </div>
         </div>
     )
 }
